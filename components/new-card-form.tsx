6d45835db86a2a00dadc6df3908985dc9219fcd6
lisettereins <lreins@tlu.ee>
2025-10-27 00:01:36 +0200

    feat: added client component for creating new card

---
+++ components/new-card-form.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { createCard } from "@/app/categories/action";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function NewCardForm({card,}: {card: {id:string, question:string, answer:string};}) {
+  const [question, setQuestion] = useState(card.question);
+  const [answer, setAnswer] = useState(card.answer);
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    await createCard(question, answer);
+    router.refresh();
+  };
+
+  return (
+    <div
+      className="mx-auto flex-col max-w-sm items-center gap-x-4 rounded-xl 
+    bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none 
+    dark:-outline-offset-1 dark:outline-white/10"
+    >
+      <h2 className="text-3xl font-bold mb-4 text-black dark:text-white">
+        Create Card
+      </h2>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="question"
+          value={question}
+          onChange={(e) => setQuestion(e.target.value)}
+          required
+          className="p-2 rounded border dark:bg-slate-700 dark:text-white"
+        />
+         <input
+          type="text"
+          placeholder="answer"
+          value={answer}
+          onChange={(e) => setAnswer(e.target.value)}
+          required
+          className="p-2 rounded border dark:bg-slate-700 dark:text-white"
+        />
+        <button
+          type="submit"
+          className="bg-gray-50 text-black p-2 rounded hover:bg-gray-200"
+        >
+          Save
+        </button>
+      </form>
+    </div>
+  );
+}
