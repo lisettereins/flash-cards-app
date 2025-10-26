bbbfcdcad01d825cf292156b2c6dc9929fd740bd
lisettereins <lreins@tlu.ee>
2025-10-26 22:53:49 +0200

    feat: added server component page for rendering NewCategoryForm

---
+++ app/categories/create/page.tsx
@@ -0,0 +1,9 @@
+
+import NewCategoryForm from "@/components/new-category-form";
+
+export default async function CreateCategoryPage() {
+  
+  return (
+      <NewCategoryForm category={{id:"", name: ""}}/>
+  );
+}
