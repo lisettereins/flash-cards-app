81e91f627bc55246d5e13693f0047284235cb0b7
lisettereins <lreins@tlu.ee>
2025-10-26 22:50:36 +0200

    feat: added client component for creating new categories

---
+++ components/new-category-form.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { createCategory } from "@/app/categories/action";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function NewCategoryForm({category,}: {category: {id:string, name:string};}) {
+  const [name, setName] = useState(category.name);
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    await createCategory(name);
+    router.push("/categories");
+    router.refresh();
+  };
+
+  return (
+    <div
+      className="mx-auto flex-col max-w-sm items-center gap-x-4 rounded-xl 
+    bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none 
+    dark:-outline-offset-1 dark:outline-white/10"
+    >
+      <h2 className="text-3xl font-bold mb-4 text-black dark:text-white">
+        Create Category
+      </h2>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="Category"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+          className="p-2 rounded border dark:bg-slate-700 dark:text-white"
+        />
+        <button
+          type="submit"
+          className="bg-gray-50 text-black p-2 rounded hover:bg-gray-200"
+        >
+          Save
+        </button>
+      </form>
+    </div>
+  );
+}
