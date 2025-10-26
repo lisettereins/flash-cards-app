3f49c03270c58d9b018671d1c8ffe34b5e274761
lisettereins <lreins@tlu.ee>
2025-10-26 23:50:40 +0200

    feat: added createCard function

--- app/categories/action.ts
+++ app/categories/action.ts
@@ -16,3 +16,18 @@ export async function createCategory(name: string) {
   }
   return { success: true };
 }
+
+export async function createCard(question: string, answer: string) {
+  const supabase = await createClient();
+
+  const { error } = await supabase
+    .from("cards")
+    .insert({ question, answer })
+    .select();
+    
+
+  if (error) {
+    throw new Error("create failed");
+  }
+  return { success: true };
+}
