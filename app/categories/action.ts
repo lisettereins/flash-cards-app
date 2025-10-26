550d06c64029f53f0789edf20a50dea33bd9a440
lisettereins <lreins@tlu.ee>
2025-10-26 22:55:17 +0200

    feat: added server action function CreateCategory for adding new category

---
+++ app/categories/action.ts
@@ -0,0 +1,18 @@
+'use server'
+
+import { createClient } from "@/lib/supabase/server";
+
+export async function createCategory(name: string) {
+  const supabase = await createClient();
+
+  const { error } = await supabase
+    .from("categories")
+    .insert({ name })
+    .select();
+    
+
+  if (error) {
+    throw new Error("create failed");
+  }
+  return { success: true };
+}
