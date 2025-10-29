32d32ef19ecffbefa89c5f8c31c101c1dd370edb
lisettereins <lreins@tlu.ee>
2025-10-29 21:58:57 +0200

    feat: added params

--- app/categories/[id]/cards/create/page.tsx
+++ app/categories/[id]/cards/create/page.tsx
@@ -2,9 +2,11 @@ import NewCardForm from "@/components/new-card-form";
 
 
 
-export default async function CreateCardPage() {
+export default async function CreateCardPage({ params }: { params: { id: string } }) {
+  const resolvedParams = await params;
   
   return (
-      <NewCardForm card={{id:"", question: "", answer: ""}}/>
+      <NewCardForm card={{id:"", question: "", answer: ""}}
+      categoryId = {resolvedParams.id}/>
   );
 }
